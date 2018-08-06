e0a9d6cefc97300c0da30846e8178e7e2a3ca184
Grégoire de Broglie <gregoire2broglie@gmail.com>
2018-08-06 22:51:46 +0200

    Programmation de la structure de la File.

--- file.h
+++ file.h
@@ -1,3 +1,14 @@
 #ifndef __FILE
 #define __FILE
+
+typedef struct File File;
+
+struct File {
+  void* element;
+  void* pointeur_tete;
+  void* pointeur_queue;
+  int taille;
+  int nb_elements;
+};
+
 #endif // __FILE
